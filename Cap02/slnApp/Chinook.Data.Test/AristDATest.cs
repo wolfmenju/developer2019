5207a393f0e71832d027d01a25a0c87ace82f4d6
wolfmenju <47580594+wolfmenju@users.noreply.github.com>
2019-02-12 19:49:07 -0500

    FF
    FF

---
+++ Cap02/slnApp/Chinook.Data.Test/AristDATest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chinook.Data.Test
+{
+    [TestClass]
+    public class ArtistDATest
+    {
+        [TestMethod]
+        public void GetCountTest()
+        {
+            var da = new ArtistDA();
+
+            Assert.IsTrue(da.GetCount() > 0);
+
+        }
+
+        [TestMethod]
+        public void GetArtistTest()
+        {
+            var da = new ArtistDA();
+
+            Assert.IsTrue(da.GetArtists().Count > 0);
+
+        }
+
+        [TestMethod]
+        public void GetArtistByNameTest()
+        {
+            var da = new ArtistDA();
+
+            Assert.IsTrue(da.GetArtists("a%").Count > 0);
+
+        }
+    }
+}
---
+++ Cap02/slnApp/Chinook.Data/ArtistDA.cs
@@ -0,0 +1,108 @@
+﻿using Chinook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data
+{
+    public class ArtistDA:BaseConnection
+    {
+        public int GetCount()
+        {
+            var result = 0;
+
+            var sql = "SELECT COUNT(1) FROM Artist";
+            /*1: Create una instancia sql DbConnection*/
+            using (IDbConnection cn = new SqlConnection(GetConnection()))
+            {
+                /*2: Create ua instancia de Command*/
+                IDbCommand cmd = new SqlCommand(sql);
+                cmd.Connection = cn;
+                cn.Open(); //Abriendo la conexion a la DB
+                           /*3. ejecutando el comando*/
+                result = (int)cmd.ExecuteScalar();
+            }
+
+            return result;
+        }
+
+        public List<Artist> GetArtists()
+        {
+            var result = new List<Artist>();
+            var sql = "SELECT ArtistId,Name FROM Artist";
+            using (IDbConnection cn = new SqlConnection(GetConnection()))
+            {
+                /*2: Create ua instancia de Command*/
+                IDbCommand cmd = new SqlCommand(sql);
+                cmd.Connection = cn;
+                cn.Open(); //Abriendo la conexion a la DB
+                           /*3. ejecutando el comando*/
+
+                var indice = 0;
+                var reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    indice = reader.GetOrdinal("ArtistId");
+                    var artistId = reader.GetInt32(indice);
+
+                    indice = reader.GetOrdinal("Name");
+                    var name = reader.GetString(indice);
+
+                    result.Add(
+                            new Artist()
+                            {
+                                ArtistId = artistId,
+                                Name = name
+                            }
+                        );
+                }
+            }
+
+            return result;
+        }
+
+        public List<Artist> GetArtists(string filterByName)
+        {
+            var result = new List<Artist>();
+            var sql = "SELECT ArtistId,Name FROM Artist WHERE Name like @name";
+            using (IDbConnection cn = new SqlConnection(GetConnection()))
+            {
+                /*2: Create ua instancia de Command*/
+                IDbCommand cmd = new SqlCommand(sql);
+                cmd.Connection = cn;
+                cn.Open(); //Abriendo la conexion a la DB
+                           /*3. ejecutando el comando*/
+
+                /*Configurando los parametros*/
+                cmd.Parameters.Add(new
+                    SqlParameter("@name", filterByName)
+                    );
+
+                var indice = 0;
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    indice = reader.GetOrdinal("ArtistId");
+                    var artistId = reader.GetInt32(indice);
+
+                    indice = reader.GetOrdinal("Name");
+                    var name = reader.GetString(indice);
+
+                    result.Add(
+                            new Artist()
+                            {
+                                ArtistId = artistId,
+                                Name = name
+                            }
+                        );
+                }
+            }
+
+            return result;
+        }
+    }
+}
